88c2e82a644758014106ddd4c83817e449733d4d
MyDesktop <dugubi1004@gmail.com>
2024-11-14 12:58:58 +0900

    1114 이지훈 - frontend url 추가

--- backend/src/util/URLs.ts
+++ backend/src/util/URLs.ts
@@ -1,5 +1,7 @@
 export const SERVER_PORT = 8000;
-export const LOCAL_URL = 'http://localhost:8000';
+export const LOCAL_URL = [
+  'http://localhost:8000',
+  'https://white-clinic.vercel.app/',
+];
 export const SERVER_URL =
   'https://whiteclinic-server-d4xcibm6r-deployproject.vercel.app/';
-export const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
